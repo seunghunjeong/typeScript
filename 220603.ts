ba40cddb3cc56bdc3ba231ad0c4f121ba9035736
jung seung hoon <todtkddj@naver.com>
2022-06-03 23:05:41 +0900

    typescript의 안정성 알기

---
+++ 220603.ts
@@ -0,0 +1,19 @@
+const nico = {
+    nickname : "nick"
+}
+
+// nico.hello() err
+
+// [1,2,3] + true err
+
+function divide(a, b) {
+    return a / b
+}
+
+// divide("hell"); err
+
+const player = {
+    age : 12
+}
+
+// player.age = false err
